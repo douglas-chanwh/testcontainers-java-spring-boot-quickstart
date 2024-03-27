e3b3a70c91189656fb5f0b5ffa58e6c436f9e642
Douglas Chan <chanwhdouglas@gmail.com>
2024-03-27 11:15:57 +0000

    Test

--- src/test/java/com/atomicjar/todos/repository/TodoRepositoryTest.java
+++ src/test/java/com/atomicjar/todos/repository/TodoRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
         "spring.test.database.replace=none",
         "spring.datasource.url=jdbc:tc:postgresql:15-alpine:///todos"
 })
+@ActiveProfiles("local")
 class TodoRepositoryTest {
 
     @Autowired
